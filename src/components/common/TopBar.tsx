08d68365c8d0470c8ebea6a6d5c27d8156ed9b8c
Jibon <mailme.jibon@gmail.com>
2024-11-19 16:42:07 +0600

    topbar updated

--- src/components/common/TopBar.tsx
+++ src/components/common/TopBar.tsx
@@ -8,20 +8,35 @@ import { TbFlagFilled } from "react-icons/tb";
 const TopBar = () => {
   return (
     <header className="bg-black/30  flex justify-between items-center">
-      <nav className="flex gap-4 container text-xs py-1 text-right w-full justify-end">
-        <Link href="#" className="text-white/80 flex gap-2 items-center">
+      <nav className="flex gap-1 container text-xs py-px text-right w-full justify-end">
+        <Link
+          href="#"
+          className="text-white/80 rounded-md hover:bg-white/15 transition-colors px-2 py-[2px] flex gap-2 items-center"
+        >
           <FaQuestionCircle className="text-sm" />
           Contact
         </Link>
-        <Link href="#" className="text-white/80 flex gap-2 items-center">
+        <span className="h-4 my-auto border-r border-dotted border-gray-400 "></span>{" "}
+        <Link
+          href="#"
+          className="text-white/80 rounded-md hover:bg-white/15 transition-colors px-2 py-[2px] flex gap-2 items-center"
+        >
           <TbFlagFilled className="text-sm" />
           English (US)
         </Link>
-        <Link href="#" className="text-white/80 flex gap-2 items-center">
+        <span className="h-4 my-auto hidden sm:flex border-r border-dotted border-gray-400 "></span>{" "}
+        <Link
+          href="#"
+          className="text-white/80 hidden sm:flex rounded-md hover:bg-white/15 transition-colors px-2 py-[2px] gap-2 items-center"
+        >
           <LiaUserPlusSolid className="text-sm" />
           Join Us
         </Link>
-        <Link href="#" className="text-white/80 flex gap-2 items-center">
+        <span className="h-4 my-auto hidden sm:flex border-r border-dotted border-gray-400 "></span>{" "}
+        <Link
+          href="#"
+          className="text-white/80 hidden sm:flex rounded-md hover:bg-white/15 transition-colors px-2 py-[2px] gap-2 items-center"
+        >
           <RiKey2Fill className="transform text-lg -rotate-90 scale-x-[-1]" />
           Sign In
         </Link>
