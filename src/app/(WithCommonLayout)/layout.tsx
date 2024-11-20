47e499f3e929c8ea6faa4e5f8211c45352b561da
Jibon <mailme.jibon@gmail.com>
2024-11-20 18:58:42 +0600

    feature grid card updated #2

--- src/app/(WithCommonLayout)/layout.tsx
+++ src/app/(WithCommonLayout)/layout.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function WithCommonLayout({
   children,
 }: Readonly<{
--- src/components/ui/FeatureGridCard.tsx
+++ src/components/ui/FeatureGridCard.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface FeatureCardProps {
   title: string;
   description: string | React.ReactNode;
